fafd5df9b0f12284947c7fcd8e8dc089f43930fd
Jinhong Chen <jinhongc8@gmail.com>
2019-02-02 11:35:57 -0500

    init

---
+++ bin/install-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install ruby-full build-essential zlib1g-dev
+gem install jekyll bundler
---
+++ bin/run-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bundle exec jekyll serve
+# http://localhost:4000
